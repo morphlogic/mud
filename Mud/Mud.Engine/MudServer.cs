0de156086db5aac40a798bf35c7c236d323b69ed
Sam Shaw <sam@samshaw.us>
2017-06-05 02:40:35 -0500

    Bit of foundation

---
+++ Mud/Mud.Engine/MudServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Extensions.Logging;
+
+namespace Mud.Engine
+{
+    public class MudServer
+    {
+        private readonly ILogger _logger;
+
+        public MudServer(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Start()
+        {
+            _logger.Info("MudServer.Start()");
+        }
+
+        public void Stop()
+        {
+            _logger.Info("MudServer.Stop()");
+        }
+    }
+}
---
+++ Mud/Mud.Host/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mud.Engine;
+using Serilog;
+using Topshelf;
+using Topshelf.Ninject;
+
+namespace Mud.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
+
+            HostFactory.Run(x =>
+            {
+                x.UseNinject();
+                x.UseSerilog();
+
+                x.Service<MudServer>(
+                    sc =>
+                    {
+                        sc.ConstructUsingNinject();
+
+                        sc.WhenStarted((s, h) =>
+                        {
+                            s.Start();
+
+                            return true;
+                        });
+
+                        sc.WhenStopped((s, h) =>
+                        {
+                            s.Stop();
+
+                            return true;
+                        });
+                    });
+            });
+        }
+    }
+}
